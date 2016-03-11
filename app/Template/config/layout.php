28c664fa66251b5cfa247f7887b97062fe6d4267
caspanh <karen_caspan04@hotmail.com>
2016-03-11 15:44:03 -0600

    arreglo responsivo2

--- app/Template/config/layout.php
+++ app/Template/config/layout.php
@@ -1,4 +1,34 @@
 <section id="main">
+	<div class="page-header">
+        <ul>
+            <li>
+                <i class="fa fa-life-ring fa-fw"></i>
+                <a href="/kanboard.1/?controller=project&amp;action=index" class="" title="">Administración del proyecto</a>
+            </li>
+        	<li>
+                <i class="fa fa-life-ring fa-fw"></i>
+             	<a href="/kanboard.1/?controller=user&amp;action=index" class="" title="">Gestión de Usuarios</a>
+            </li>
+
+            <li>
+            	<i class="fa fa-life-ring fa-fw"></i>
+            	<a href="/kanboard.1/?controller=app&amp;action=index" class="" title="Tablero"> inicio </a>
+
+            </li>
+
+            <li>
+                <i class="fa fa-life-ring fa-fw"></i>
+                <a href="/kanboard.1/?controller=project&amp;action=index" class="" title="">Administración del proyecto</a>
+            </li>
+            <li>
+                <i class="fa fa-life-ring fa-fw"></i>
+                <a href="/kanboard.1/?controller=project&amp;action=index" class="" title="">Administración del proyecto</a>
+            </li>
+
+
+        </ul>
+    </div>
+
     <section class="sidebar-container" id="config-section">
 
         <?= $this->render('config/sidebar') ?>
